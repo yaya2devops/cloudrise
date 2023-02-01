842f8bb04261693cf052583c9eaf91bc40601d12
YAYA <yahyaabulhaj@outlook.com>
2023-02-01 22:31:53 +0100

    better stuff!

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,7 +36,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="me.png"
+        src="iloveit.png"
         width={50}
         height={50}
         quality={95}
